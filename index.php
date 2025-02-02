b9f315a670ad9ccfe4a888b21a3dfba91106965b
OomsEwoud <ewoud.ooms@student.kdg.be>
2025-02-02 23:42:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
+include './vendor/autoload.php';
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
 include "./snippets/layout/header.php";
 ?>
-
 <?php
 include "./snippets/layout/footer.php";
 ?>
